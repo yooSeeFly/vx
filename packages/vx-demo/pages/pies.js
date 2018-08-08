b7576167c27ae75ef3aecd052948b227ebee95e9
hshoff <hi@hshoff.com>
2018-08-08 15:36:22 -0400

    [shape][breaking] refactor <Pie /> optional children function

--- packages/vx-demo/pages/pies.js
+++ packages/vx-demo/pages/pies.js
@@ -15,27 +15,23 @@ export default () => {
       component={Pie}
       title="Pies"
     >
-      {`import React from 'react';
-import { Pie } from '@vx/shape';
+      {`import { GradientPinkBlue } from '@vx/gradient';
 import { Group } from '@vx/group';
-import { GradientPinkBlue } from '@vx/gradient';
-import { letterFrequency, browserUsage } from '@vx/mock-data';
+import { browserUsage, letterFrequency } from '@vx/mock-data';
+import { Pie } from '@vx/shape';
+import React from 'react';
 
 const letters = letterFrequency.slice(0, 4);
 const browsers = Object.keys(browserUsage[0])
-  .filter(k => k !== "date")
+  .filter(k => k !== 'date')
   .map(k => ({ label: k, usage: browserUsage[0][k] }));
+
+const usage = d => d.usage;
+const frequency = d => d.frequency;
 
 function Label({ x, y, children }) {
   return (
-    <text
-      fill="white"
-      textAnchor="middle"
-      x={x}
-      y={y}
-      dy=".33em"
-      fontSize={9}
-    >
+    <text fill="white" textAnchor="middle" x={x} y={y} dy=".33em" fontSize={9}>
       {children}
     </text>
   );
@@ -44,59 +40,67 @@ function Label({ x, y, children }) {
 export default ({
   width,
   height,
-  events = false,
   margin = {
     top: 30,
     left: 20,
     right: 20,
-    bottom: 110,
+    bottom: 110
   }
 }) => {
-  if (width < 10) return null;
   const radius = Math.min(width, height) / 2;
+
   return (
     <svg width={width} height={height}>
       <GradientPinkBlue id="gradients" />
-      <rect
-        x={0}
-        y={0}
-        rx={14}
-        width={width}
-        height={height}
-        fill="url('#gradients')"
-      />
+      <rect x={0} y={0} rx={14} width={width} height={height} fill="url('#gradients')" />
       <Group top={height / 2 - margin.top} left={width / 2}>
         <Pie
           data={browsers}
-          pieValue={d => d.usage}
+          pieValue={usage}
           outerRadius={radius - 80}
           innerRadius={radius - 120}
-          fill="white"
-          fillOpacity={d => 1 / (d.index + 2) }
           cornerRadius={3}
           padAngle={0}
-          centroid={(centroid, arc) => {
-            const [x, y] = centroid;
-            const { startAngle, endAngle } = arc;
-            if (endAngle - startAngle < .1) return null;
-            return <Label x={x} y={y}>{arc.data.label}</Label>;
+        >
+          {pie => {
+            return pie.arcs.map((arc, i) => {
+              const [x, y] = pie.path.centroid(arc);
+              const { startAngle, endAngle } = arc;
+              return (
+                <g key={\`pie1-arc-\${i}\`}>
+                  <path d={pie.path(arc)} fill={'#FFFFFF'} fillOpacity={1 / (i + 2)} />
+                  {endAngle - startAngle >= 0.1 && (
+                    <Label x={x} y={y}>
+                      {arc.data.label}
+                    </Label>
+                  )}
+                </g>
+              );
+            });
           }}
-        />
+        </Pie>
         <Pie
           data={letters}
-          pieValue={d => d.frequency}
+          pieValue={frequency}
           outerRadius={radius - 135}
           fill="black"
-          fillOpacity={d => 1 / (d.index + 2) }
+          fillOpacity={0.4}
+          stroke={'white'}
+          strokeWidth={2}
+          strokeOpacity={0.1}
           centroid={(centroid, arc) => {
             const [x, y] = centroid;
-            return <Label x={x} y={y}>{arc.data.letter}</Label>;
+            return (
+              <Label x={x} y={y}>
+                {arc.data.letter}
+              </Label>
+            );
           }}
         />
       </Group>
     </svg>
   );
-}`}
+};`}
     </Show>
   );
 };
